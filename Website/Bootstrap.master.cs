e12f11ef7632f39404714dcc4f5334e18f9c82ad
D6C92FE5 <D6C92FE5@hotmail.com>
2013-05-24 19:40:44 +0800

    移除未使用和非标准的meta标签

--- Website/Bootstrap.master.cs
+++ Website/Bootstrap.master.cs
@@ -10,5 +10,8 @@ public partial class Bootstrap : System.Web.UI.MasterPage
     protected void Page_Load(object sender, EventArgs e)
     {
         Page.Header.DataBind();
+
+        // 让 IE 使用最新的最新的引擎渲染网页
+        Response.AppendHeader("X-UA-Compatible", "IE=edge,chrome=1");
     }
 }
